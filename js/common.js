1ed52c126dd6ebf563c54aebad715e0d86cb6f84
website-master <hotmart13@mail.ru>
2017-05-13 09:34:31 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,84 @@
+$(document).ready(function()  {
+	//popup
+		$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true,
+		showCloseBtn: true
+	});
+
+		$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+	//menu
+	$(".toggle-mnu").click(function() {
+  $(this).toggleClass("on");
+  $(".main-mnu").slideToggle();
+  return false;
+});
+
+	$(document).ready(function(){
+
+        var $menu = $("#menu");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.removeClass("fixed").addClass("default");
+            }
+        });//scroll
+    });
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+	//prokrytka menu
+	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 1000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+//animate
+	
+	$(".desk").animated("bounce");
+	$(".zayavka").animated("swing");
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
